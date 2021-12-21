07880dc84733e1578080f8815e8fcb96183b54bc
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-12-21 09:54:02 -0500

    Finished function to remove and fix columns

--- clean.py
+++ clean.py
@@ -13,12 +13,12 @@ def process_file(path):
 
 
 def understand_data(df):
-    print('\nUnderstanding Data Section')
-    print('\n', df.head())
-    print('\n', df.dtypes)
+    print('\n**** Understanding Data Section ****')
+    print('\nData head:\n', df.head())
+    print('\nData types of columns:\n', df.dtypes)
     df.year = pd.to_numeric(df.year, errors='coerce')
     df.height = pd.to_numeric(df.height, errors='coerce')
-    print('\n', df.dtypes)
+    print('\nData types following conversion:\n', df.dtypes)
     print('\nMin year: ', df['year'].min())
     print('\nMax year: ', df['year'].max())
     print('\nAvg year: ', df['year'].mean())
@@ -28,9 +28,26 @@ def understand_data(df):
     print('\nExamine columns with word year (case insensitive):\n', df.filter(regex="(?i)year"))
 
 
+def adjust_columns(df):
+    print('\n\n**** Removing and Fixing Columns Section ****')
+    print('\nDrop first row (Not in place):\n', df.drop(0))
+    print('\nDrop id column:\n', df.drop('id', axis=1))
+    print('\nDrop multiple columns:\n', df.drop(columns=['id', 'year', 'height', 'artistRole', 'creditLine']))
+    print('\nDrop multiple rows:\n', df.drop(labels=[0, 1, 2]))
+    print('\nData columns:\n', df.columns)
+    print('\nData columns to lower (Not in place):\n', df.columns.str.lower())
+    data1 = pd.read_csv('artwork_sample.csv')
+    data1.columns = map(lambda x: x.lower(), data1.columns)
+    print('\nColumns to lowercase using function:\n', data1.columns)
+    data1.rename({"thumbnailUrl":"thumbnail"}, axis=1, inplace=True)
+    data1.columns = ['id', 'accessionNumber', 'artist', 'artistRole', 'artistId', 'title', 'dateText', 'medium', 'creditLine', 'year', 'acquisitionYear', 'dimensions', 'width', 'height', 'depth', 'units', 'inscription', 'thumbnailCopyright', 'thumbnail', 'url']
+    print('\nRenamed columns:\n', data1.columns)
+
+
 def main():
     data = process_file('artwork_sample.csv')
     understand_data(data)
+    adjust_columns(data)
 
 
 main()
